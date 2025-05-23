5ecb74a8558b801840f22dca6b354bed332e9aa6
sw4m <56038988+sw4m@users.noreply.github.com>
2025-05-23 18:53:05 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -3,3 +3,7 @@ RegisterNetEvent('um-idcard-npc:server:AddItemtoExport', function(args)
     exports['um-idcard']:CreateMetaLicense(src, args.itemName)
     TriggerClientEvent('um-idcard-npc:client:OxNotify', src, args.itemName)
 end)
+
+lib.callback.register('um-idcard-menu:hasPermission', function(source, meta)
+    return exports.qbx_core:GetPlayer(source).PlayerData.metadata.licences[meta]
+    end)
